f09f1f817296cdeb211767b0bd2fd70b01b82db3
shasherazi <hassanrandomz@gmail.com>
2023-02-22 13:54:34 +0500

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Books from './components/Books';
+import Categories from './components/Categories';
+
 const App = () => (
-  <h1>Hello</h1>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Books />} />
+      <Route path="categories" element={<Categories />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
