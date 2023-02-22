85ab45d61a92fd9a69359853fb016f84ed50c895
shasherazi <hassanrandomz@gmail.com>
2023-02-22 14:33:57 +0500

    Add Nav component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Books from './components/Books';
 import Categories from './components/Categories';
+import Nav from './components/Nav';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Books />} />
-      <Route path="categories" element={<Categories />} />
+      <Route element={<Nav />}>
+        <Route path="/" element={<Books />} />
+        <Route path="categories" element={<Categories />} />
+      </Route>
     </Routes>
   </BrowserRouter>
 );
